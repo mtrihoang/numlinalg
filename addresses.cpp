d92f76393f4b929a3d18648e07e8ed86ac90d453
mtrihoang <trihoang30ec@gmail.com>
2026-01-31 03:17:34 +0100

    Replace all files in test by those in main

--- addresses.cpp
+++ addresses.cpp
@@ -3,16 +3,16 @@
 using namespace std;
 
 int main() {
-  vector<double> v = {1., 2., 3., 4., 5.};
+  vector<double> v = {5., 4., 3., 2., 1.};
   cout << "Traditional for loop:" << endl;
-  for (size_t i = 0; i < v.size(); ++i) {
+  for (size_t i = 0; i < v.size(); i++) {
     cout << "Address of element " << i + 1 << " = " << &v[i] << endl;
   }
 
   cout << "\nRange-based for loop" << endl;
   int j = 0;
-  for (double& e : v) {
-    cout << "Address of element " << j + 1 << " = " << &v[j] << endl;
+  for (double& c : v) {
+    cout << "Address of element " << j + 1 << " = " << &c << endl;
     ++j;
   }
 }
