ef5c252d5ae626495cd65413234a28dbd6431d8c
mtrihoang <trihoang30ec@gmail.com>
2026-02-01 09:03:52 +0100

    Add an alternative way to dereference pointers

--- pointers.cpp
+++ pointers.cpp
@@ -6,5 +6,16 @@ int main() {
   double* p;  // dataType* p
   double i = 1.1;
   p = &i;
-  cout << "Address of " << i << ": " << p << endl;
+  cout << "Address of p (" << p << "): " << &p << endl;
+  cout << "Value stored in p (i)" << ": " << *p << endl;
+  cout << "Value stored in p (i)" << ": " << p[0] << endl;
+  cout << "Address of i (" << i << "): " << p << endl;
+
+  double* h = 0;
+  double j = 1.7;
+  h = &j;
+  cout << "\nAddress of h (" << h << "): " << &h << endl;
+  cout << "Value stored in h (j)" << ": " << *h << endl;
+  cout << "Value stored in h (j)" << ": " << h[0] << endl;
+  cout << "Address of j (" << j << "): " << &j << endl;
 }
